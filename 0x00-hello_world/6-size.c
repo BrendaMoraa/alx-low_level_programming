db1c27313818430bff7ec5b72a47fee950dfb01a
BrendaMoraa <moraab30@gmail.com>
2023-06-15 09:52:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints the size of various computer types
  *
- * Return 0 (Success)
+ * Return: Always  0 (Success)
  */
 int main(void)
 {
