7b257532755b4cea1837ac358df3ccf503fc7231
BrendaMoraa <moraab30@gmail.com>
2023-06-15 11:10:49 -0700

    101-quote.c '

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
+* main - A program that prints exact text followed by a new line to the standart error
 *
 * Return: Always 1 (Success)
 */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
