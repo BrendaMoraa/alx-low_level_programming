17f8ef8d9a5c4c005a394c4266368f4ccb478dc8
BrendaMoraa <moraab30@gmail.com>
2023-06-21 13:17:29 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char q;
+
+	for (q = 'a'; q <= 'z'; q++)
+		_putchar(c);
+	_putcahr('\n')
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+	/**
+	 * print_alphabet_10 - prints 10 times the alphabet lowercase
+	 * followed by a new line allways
+	 */
+	void print_alphabet_10(void)
+	{
+		char ch;
+		int i;
+
+
+		i = 0;
+
+
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
