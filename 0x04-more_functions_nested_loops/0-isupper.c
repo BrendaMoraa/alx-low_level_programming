0aedd3872f51d8c8a33d6a75e9279ea89bcd952b
BrendaMoraa <moraab30@gmail.com>
2023-06-22 09:07:37 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @q: the number to be checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int q)
+{
+	if (q >= 'A' && q <= 'Z')
+		return (1);
+	else
+		return (0);
+}
